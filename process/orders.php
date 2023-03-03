f7f46ed7794084c150df126c4ed5c429157f6ea1
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-03-03 16:02:18 -0300

    pulling the pizzas in the database

--- process/orders.php
+++ process/orders.php
@@ -1,2 +1,24 @@
 <?php
 include_once("conn.php");
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === "GET") {
+  $pedidosQuery = $conn->query("SELECT * FROM pedidos;");
+
+  $pedidos = $pedidosQuery->fetchAll();
+
+  $pizzas = [];
+
+  foreach ($pedidos as $pedido) {
+    $pizza = [];
+    // Resgatando a pizza
+    $pizza["id"] = $pedido["pizza_id"];
+    $pizzaQuery = $conn->prepare("SELECT * FROM pizzas WHERE id = :pizza_id");
+    $pizzaQuery->bindParam(":pizza_id", $pizza["id"]);
+    $pizzaQuery->execute();
+
+    $pizzaData = $pizzaQuery->fetch(PDO::FETCH_ASSOC);
+  }
+} else if ($method === "POST") {
+}
