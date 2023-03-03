9cce0c56affbf9500cac62572aa9645268fdfb26
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-03-03 15:35:58 -0300

    finish html and css of dashboard page

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,57 @@
 <?php
 include_once("templates/header.php");
 ?>
-<h1>a</h1>
+<div class="main-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2>Gerenciar Pedidos</h2>
+      </div>
+      <div class="col-md-12 table-container">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col"><span>Pedido</span> #</th>
+              <th scope="col"><span>Borda</span> #</th>
+              <th scope="col"><span>Massa</span> #</th>
+              <th scope="col"><span>Sabores</span> #</th>
+              <th scope="col"><span>Ações</span> #</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>#1</td>
+              <td>Cheddar</td>
+              <td>Catupiry</td>
+              <td>Quatro Queijos</td>
+              <td>
+                <form action="process/orders.php" method="POST" class="form-group update-form">
+                  <input type="hidden" name="type" value="update">
+                  <input type="hidden" name="id" value="1">
+                  <select name="status" class="form-control status-input">
+                    <option value="">Entrega</option>
+                  </select>
+                  <button type="submit" class="update-btn">
+                    <i class="fas fa-sync-alt"></i>
+                  </button>
+                </form>
+              </td>
+              <td>
+                <form action="process/orders.php" method="POST">
+                  <input type="hidden" name="type" value="delete">
+                  <input type="hidden" name="id" value="1">
+                  <button type="submit" class="delete-btn">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </form>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
 <?php
 include_once("templates/footer.php");
 ?>
--- process/pizza.php
+++ process/pizza.php
@@ -30,31 +30,31 @@ if ($method === "GET") {
     $_SESSION["msg"] = "Selecione no máximo 3 sabores!";
     $_SESSION["status"] = "warning";
   } else {
-    // salvando borda e massa na pizza
+    // Salvando borda e massa na pizza
     $stmt = $conn->prepare("INSERT INTO pizzas (borda_id, massa_id) VALUES (:borda, :massa)");
-    // filtrando inputs
+    // Filtrando inputs
     $stmt->bindParam(":borda", $borda, PDO::PARAM_INT);
     $stmt->bindParam(":massa", $massa, PDO::PARAM_INT);
     $stmt->execute();
 
-    // resgatando último id da última pizza
+    // Resgatando último id da última pizza
     $pizzaId = $conn->lastInsertId();
 
     $stmt = $conn->prepare("INSERT INTO pizza_sabor (pizza_id, sabor_id) VALUES (:pizza, :sabor)");
-    // repetição até terminar de salvar todos os sabores
+    // Repetição até terminar de salvar todos os sabores
     foreach ($sabores as $sabor) {
-      // filtrando os inputs
+      // Filtrando os inputs
       $stmt->bindParam(":pizza", $pizzaId, PDO::PARAM_INT);
       $stmt->bindParam(":sabor", $sabor, PDO::PARAM_INT);
       $stmt->execute();
     }
-    // criar o pedido da pizza
+    // Criar o pedido da pizza
     $stmt = $conn->prepare("INSERT INTO pedidos (pizza_id, status_id) VALUES (:pizza, :status)");
 
-    // status -> sempre inicia com 1, que é em produção
+    // Status -> sempre inicia com 1, que é em produção
     $statusId = 1;
 
-    // filtrar inputs
+    // Filtrar inputs
     $stmt->bindParam(":pizza", $pizzaId);
     $stmt->bindParam(":status", $statusId);
 
